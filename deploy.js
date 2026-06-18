0156aab5db5b29f90deda30d7ae8c77d6557b173
blackmalapaga <145885794+blackmalapaga@users.noreply.github.com>
2026-06-18 19:02:44 -0400

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("lfd")
+    .setDescription("Buscando jugadores system")
+].map(command => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Deploying slash commands...");
+
+    await rest.put(
+      Routes.applicationCommands("YOUR_BOT_CLIENT_ID"),
+      { body: commands }
+    );
+
+    console.log("Commands registered!");
+  } catch (err) {
+    console.error(err);
+  }
+})();
