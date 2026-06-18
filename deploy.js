56c95e69402b1d496c829b105e9c137bbae1db37
blackmalapaga <145885794+blackmalapaga@users.noreply.github.com>
2026-06-18 19:14:54 -0400

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,24 +1,26 @@
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("lfd")
-    .setDescription("Buscando jugadores system")
-].map(command => command.toJSON());
+const TOKEN = process.env.TOKEN;
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const commands = [
+    new SlashCommandBuilder()
+        .setName("lfd")
+        .setDescription("Buscando jugadores system")
+].map(c => c.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 (async () => {
-  try {
-    console.log("Deploying slash commands...");
+    try {
+        console.log("Deploying slash commands...");
 
-    await rest.put(
-      Routes.applicationCommands("1517292944829448363"),
-      { body: commands }
-    );
+        await rest.put(
+            Routes.applicationCommands("1517292944829448363"),
+            { body: commands }
+        );
 
-    console.log("Commands registered!");
-  } catch (err) {
-    console.error(err);
-  }
+        console.log("Commands registered!");
+    } catch (err) {
+        console.error(err);
+    }
 })();
